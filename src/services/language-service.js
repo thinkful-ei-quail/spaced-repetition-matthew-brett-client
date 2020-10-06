ecd4110f178a6b1e0b26a8bc492fe69565a3d8e7
Brett Westerlund <drbretto82@gmail.com>
2020-10-06 11:22:56 -0400

    added language api

---
+++ src/services/language-service.js
@@ -0,0 +1,15 @@
+import config from "../config";
+import TokenService from "./token-service";
+
+const ApiService = {
+  getLanguage() {
+    return fetch(`${config.API_ENDPOINT}/language/`, {
+      headers: {
+        "authorization": `bearer ${TokenService.getAuthToken()}`,
+      },
+    }).then((res) =>
+      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+    );
+  },
+};
+export default ApiService;
